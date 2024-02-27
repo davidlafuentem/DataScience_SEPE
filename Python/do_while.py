19bfb5b04e05ba84254473235dd130a007787bba
davidlafuentem <45320142+davidlafuentem@users.noreply.github.com>
2024-02-27 21:02:17 +0100

    Delete Python/do_while.py

--- Python/do_while.py
+++
@@ -1,9 +0,0 @@
-testigo = True
-
-while( testigo == True):
-    testigo = False
-
-    numero = input("Introduce un número positivo mayor")
-    if numero <=0:
-        print("El número no es válido, repítelo")
-        testigo = True
